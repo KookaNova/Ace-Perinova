eb1d70bb6359de209faba3225397981a540ee88b
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2022-04-12 01:29:16 -0600

    Glowing UI shader added, target indicator utility added

--- AcePerinova/Assets/Spacecraft Controller/IndicatorComponent.cs
+++ AcePerinova/Assets/Spacecraft Controller/IndicatorComponent.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class IndicatorComponent : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace AcePerinova.Utilities{
+    public class IndicatorComponent : MonoBehaviour
     {
-        
-    }
+        public Text player, character, distance;
+        public GameObject lockIndicator;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private void Awake() {
+            lockIndicator.SetActive(false);
+        }
+    
     }
 }
+
