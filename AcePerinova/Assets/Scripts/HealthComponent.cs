07630a53a5871633272c9f112047735a5069c20e
Connor Cox <46508202+ConnorCoxAGD@users.noreply.github.com>
2022-04-22 22:27:13 -0600

    Updated material, created canopy shader

---
+++ AcePerinova/Assets/Scripts/HealthComponent.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthComponent : MonoBehaviour
+{
+    public float health, shield, chargeRate, chargeDelay;
+
+    public void TakeDamage(){
+
+    }
+}
